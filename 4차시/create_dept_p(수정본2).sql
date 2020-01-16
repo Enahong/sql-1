c26061b6d3e3c27724d1b1d22c7af5ed7fa3d4ad
chariehong <charieh@daum.net>
2020-01-16 13:38:27 +0900

    Add files via upload

---
+++ 4차시/create_dept_p(수정본2).sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE PROCEDURE create_dept_p ( p_dept_id NUMBER,
+                                            p_dept_nm VARCHAR2,
+                                            p_man_id  NUMBER,
+                                            p_loc_id  NUMBER )
+IS
+  v_cnt  NUMBER;
+BEGIN
+    -- 부서번호 체크 
+    SELECT COUNT(*)
+      INTO v_cnt
+      FROM departments
+     WHERE department_id = p_dept_id;
+     
+    -- v_cnt가 0보다 크면 이미 존재하는 부서임 
+    IF v_cnt > 0 THEN
+       RAISE_APPLICATION_ERROR (-20201, p_dept_id || '번 부서가 이미 존재합니다');
+       RETURN; -- 프로시저에서 return 만나면 이후 로직 수행 안하고 빠져나감 
+    END IF;
+    
+    -- location_id 체크 
+    SELECT COUNT(*)
+      INTO v_cnt
+      FROM locations
+     WHERE location_id = p_loc_id;
+     
+    -- v_cnt가 0이면 locations 테이블에 해당 location_id가 없으므로 오류임
+    IF v_cnt = 0 THEN
+       RAISE_APPLICATION_ERROR (-20201, p_loc_id || '번이 locations 테이블에 없습니다');
+       RETURN; -- 프로시저에서 return 만나면 이후 로직 수행 안하고 빠져나감 
+    END IF;     
+         
+    -- 부서정보 입력 
+	INSERT INTO departments (department_id, department_name, manager_id, location_id)
+	VALUES ( p_dept_id, p_dept_nm, p_man_id, p_loc_id );
+	
+	COMMIT;
+	
+EXCEPTION WHEN OTHERS THEN
+  RAISE_APPLICATION_ERROR (-20205, SQLERRM);
+  ROLLBACK;
+END; 
